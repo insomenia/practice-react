8016dae85e99a1b4a384ae84366720d0a2d4a057
ehdgur3125 <ehdgur3125@naver.com>
2021-04-09 10:39:41 +0900

    [CSS]fix overall

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -29,44 +29,42 @@ const HomePage = (props) => {
         </NavLeft>
       </Navbar>
       {/* Page content */}
-      <div className="p-3">
-        <List mediaList>
+      <div className="p-3 grid grid-cols-2 justify-items-center">
+        {/*<List mediaList>*/}
         {categoryId>=0
         ?items.map((item,index)=>
-        <ListItem
+        /*<ListItem
           key={item.itemId}
           link={`/item/${item.itemId}`}
           title={item.name}
           subtitle={`찜:${item.likes} 판매량:${item.purchases}`}
           text={`${item.minPrice}원 ~ ${item.maxPrice}원`}>
-          <img slot='media' src={`http://localhost:3000/img/small/${item.name}`} className='w-40 h-40 border border-gray'/>
-        </ListItem>
-          /*<a key={item.itemId} href={`/item/${item.itemId}`}>
-            <Row noGap className={`text-center text-base border-b border-r border-l border-gray${index===0?' border-t':''} items-stretch`}>
-              <Col width="30" className="border border-gray">
-                <img src={`http://localhost:3000/img/small/${item.name}`} className='w-full h-full border-r border-gray'/>
-              </Col>
-              <Col width="70" className="border border-gray">
-                <Row className='items-center h-12'>
-                  <Col width='50'>{item.name}</Col>
-                  <Col width='20'>
-                    <Row><Col>찜</Col></Row>
-                    <Row><Col>{item.likes}</Col></Row>
-                  </Col>
-                  <Col width='30'>
-                    <Row><Col>판매량</Col></Row>
-                    <Row><Col>{item.purchases}</Col></Row>
-                  </Col>
-                </Row>
-                <Row className='border h-20 items-center'>
-                  <Col>{item.minPrice}-{item.maxPrice}</Col>
-                </Row>
+        <img slot='media' src={`http://localhost:3000/img/small/${item.name}`} className='w-32 h-32 border border-gray'/>
+        </ListItem>*/
+          <a key={item.itemId} href={`/item/${item.itemId}`} className='w-full text-center text-base border pt-5'>
+            <Row>
+              <Col>
+                <img src={`http://localhost:3000/img/small/${item.name}`} className='w-full h-full border-b'/>
               </Col>
             </Row>
-          </a>*/
+            <Row className='text-lg border-b'>
+              <Col>{item.name}</Col>
+            </Row>
+            <Row>
+              <Col width='60'>찜</Col>
+              <Col width='40'>{item.likes}</Col>
+            </Row>
+            <Row>
+              <Col width='60'>판매량</Col>
+              <Col width='40'>{item.purchases}</Col>
+            </Row>
+            <Row className='border-t'>
+              <Col>{item.minPrice}원 ~ {item.maxPrice}원</Col>
+            </Row>
+          </a>
         )
         :null}
-        </List>
+        {/*</List>*/}
       </div>
 
     </Page>
